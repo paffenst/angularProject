ec2b724d2e650c3dfac3ddab7dc4b98327705ca9
paffenst <paffen.st@gmail.com>
2019-03-07 23:02:51 +0100

    UPDATE bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { from } from 'rxjs';
+import { ListEmployeesComponent } from './employee/list-employees.component';
+import { CreateEmployeeComponent } from './employee/create-employee.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: 'list', component: ListEmployeesComponent },
+  { path: 'create', component: CreateEmployeeComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes), CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
